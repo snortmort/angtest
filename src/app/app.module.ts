9e57da3892a99e857d3448e432a4b2bf098ce71c
morten <hatlenmorten@gmail.com>
2021-04-14 15:02:20 +0200

    commit and push test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
@@ -12,8 +13,9 @@ import { MessageService } from "./message.service";
 import { AppRoutingModule } from "./app-routing.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule,],
   declarations: [
     AppComponent,
     HelloComponent,
