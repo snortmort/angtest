d0c9247e0111b530efcc58d7d54c555a294bebee
unknown <hatlenmorten@gmail.com>
2021-04-27 14:31:20 +0200

    finshed tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
@@ -12,17 +13,27 @@ import { MessagesComponent } from "./messages/messages.component";
 import { MessageService } from "./message.service";
 import { AppRoutingModule } from "./app-routing.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule,],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),],
   declarations: [
     AppComponent,
     HelloComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   bootstrap: [AppComponent],
   providers: [HeroService, MessageService]
